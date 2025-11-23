73e91f1c299613a596ac503bc7c774341fead556
VORN-studio <officiallunbrend@gmail.com>
2025-11-23 22:39:56 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function earn() {
+    alert("Earn function coming soon!");
+}
+
+function withdraw() {
+    alert("Withdraw system coming soon!");
+}
+
+function invite() {
+    alert("Referral system coming soon!");
+}
+
+function profile() {
+    alert("Profile coming soon!");
+}
