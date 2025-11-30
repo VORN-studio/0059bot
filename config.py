f3c0916b5e91c48c38ac5b49cd1d3e6b418146e9
Narek 059 <officiallunbrend@gmail.com>
2025-11-30 23:04:01 +0400

    ubdate

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+BOT_TOKEN = os.environ.get("8001785392:AAFlfF-SkcJJqG52GCsWT7calY9YLe1aqGw")       # Render / Railway-ից կգա
+DB_URL = os.environ.get("DB_URL")             # Neon / Supabase / Railway DB
--- static/app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-
-    // Show loading for 1.5 seconds, then show the main app
-    setTimeout(() => {
-        document.getElementById("loading-screen").classList.add("hidden");
-        document.getElementById("main-app").classList.remove("hidden");
-    }, 1500);
-
-});
