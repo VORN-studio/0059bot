68c894d43e781db387feb333f67f6c62427b6148
Narek 059 <officiallunbrend@gmail.com>
2025-12-06 15:19:56 +0400

    ubdate

--- webapp/tasks/tasks.js
+++ webapp/tasks/tasks.js
@@ -1,3 +1,17 @@
 function closeTasks() {
     window.location.href = "../index.html";
 }
+
+function hideAll() {
+    document.querySelectorAll(".screen").forEach(s => {
+        s.style.display = "none";
+    });
+}
+
+function openCategory(cat) {
+    hideAll();
+    document.getElementById("screen-" + cat).style.display = "block";
+}
+
+// default
+hideAll();
