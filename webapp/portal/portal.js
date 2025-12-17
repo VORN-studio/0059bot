d35c7a57e8f7eab3bb5ded68272c85d8038d4edd
Narek 059 <officiallunbrend@gmail.com>
2025-12-17 17:30:40 +0400

    ubdate

--- webapp/portal/portal.js
+++ webapp/portal/portal.js
@@ -1922,7 +1922,9 @@ function renderChatMessage(msg, isMe = false, isDM = false) {
             </div>
         </div>
         
-        <div class="message-reactions" id="reactions-${messageId}" style="display:none;"></div>
+        <div style="text-align:${align};">
+            <div class="message-reactions" id="reactions-${messageId}" style="display:none;"></div>
+        </div>
     </div>`;
 }
 
